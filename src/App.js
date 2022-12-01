03d9ebb449c2354cf2608658608bb57daf3eb2a2
German Orieta <ger.orieta@gmail.com>
2022-12-01 20:11:08 -0300

    primer-entrega

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar'
+import Carousel from './components/Carousel/Carousel'
+import Card from './components/Card/Card'
+
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      <Carousel/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +25,12 @@ function App() {
           Learn React
         </a>
       </header>
+      <Card
+      titulo={"Selva Negra"}
+      descripcion={"La tarta o torta Selva Negra es una tarta típica de la cocina de Baden y uno de los dulces más característicos de la gastronomía alemana. Está compuesta por varias capas de bizcochuelo de chocolate embebido en kirsch e intercaladas con nata y cerezas."}
+      img={"https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Schwarzwalder_Kirschtorte.jpg/1200px-Schwarzwalder_Kirschtorte.jpg"}
+      btnText={"Ver Mas"}
+      />
     </div>
   );
 }
